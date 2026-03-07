f365e79534fc66306c80e3b8938de8737af79789
Eschryn <git.elpul@eschryn.aleeas.com>
2026-03-07 20:40:59 +0100

    Simple bot class to get started - this probably will later manage chaches etc

---
+++ Fluxify.Bot/Bot.cs
@@ -0,0 +1,18 @@
+﻿using Fluxify.Commands;
+using Fluxify.Core;
+using Fluxify.Gateway;
+
+namespace Fluxify.Bot;
+
+public class Bot(string prefix, FluxerConfig config)
+{
+    public CommandCollection Commands { get; } = new();
+    public GatewayClient Gateway { get; } = new(config);
+
+    public async Task RunAsync(BotTokenCredentials credentials)
+    {
+        var dispatcher = Commands.BuildDispatcher(prefix);
+        Gateway.MessageCreate += dispatcher.DispatchAsync;
+        await Gateway.RunAsync(credentials);
+    }
+}
