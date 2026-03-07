86238d14d4c336ca14ad2ddcd911e394db276e9d
Eschryn <git.elpul@eschryn.aleeas.com>
2026-03-07 22:26:48 +0100

    Fix GatewayTypingStart event

--- Fluxify.Gateway/Model/Data/Channel/GatewayTypingStart.cs
+++ Fluxify.Gateway/Model/Data/Channel/GatewayTypingStart.cs
@@ -1,11 +1,14 @@
+using System.Text.Json.Serialization;
 using Fluxify.Core;
 using Fluxify.Dto.Guilds.Members;
+using Fluxify.Gateway.Json;
 
-namespace Fluxify.Gateway.Model.Dto;
+namespace Fluxify.Gateway.Model.Data.Channel;
 
 public record GatewayTypingStart(
     Snowflake ChannelId,
     Snowflake UserId,
+    [property: JsonConverter(typeof(JsonEpochTimeConverter))] 
     DateTimeOffset Timestamp,
     Snowflake? GuildId,
     GuildMember? Member
