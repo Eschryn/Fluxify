a3c258a3ea83bc91a4bde811c065e8e83f83ba4d
Eschryn <8217655+Eschryn@users.noreply.github.com>
2026-03-11 02:32:56 +0100

    Update Example.cs

--- Example.cs
+++ Example.cs
@@ -6,16 +6,19 @@
 // you need to provide the FLUXIFY_BOT_TOKEN variable with the bot token
 
 using Fluxify.Bot;
-using Fluxify.Commands;
+using Fluxify.Commands.CommandCollection;
 using Fluxify.Core;
 using Microsoft.Extensions.Logging;
 
-var token = new BotTokenCredentials(Environment.GetEnvironmentVariable("FLUXIFY_BOT_TOKEN")
-                                    ?? throw new Exception("FLUXIFY_BOT_TOKEN environment variable is not set"));
 var loggerFactory = LoggerFactory.Create(b => b.AddConsole().SetMinimumLevel(LogLevel.Trace));
+var config = new FluxerConfig(loggerFactory)
+{
+    Credentials = new BotTokenCredentials(Environment.GetEnvironmentVariable("FLUXIFY_BOT_TOKEN")
+                                          ?? throw new Exception("FLUXIFY_BOT_TOKEN environment variable is not set"))
+};
 
-var bot = new Bot("f!", new FluxerConfig(loggerFactory));
+var bot = new Bot("f!", config);
 
 bot.Commands.Command("ping", () => Console.WriteLine("pong"));
 
-await bot.RunAsync(token);
+await bot.RunAsync();
