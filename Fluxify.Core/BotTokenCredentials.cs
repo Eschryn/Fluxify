1279e3577ca4f5d897a46c0aeb45bde7e9ed6e63
Eschryn <git.elpul@eschryn.aleeas.com>
2026-03-15 00:17:29 +0100

    Streamline authorization header value for future bearer token support

--- Fluxify.Core/BotTokenCredentials.cs
+++ Fluxify.Core/BotTokenCredentials.cs
@@ -2,10 +2,13 @@
 
 public class BotTokenCredentials(string token)
 {
+    public const string TypeConst = "Bot";
     public string Token { get; } = token;
     
     public bool Validate()
     {
         return true;
     }
+    
+    public string GetAuthorizationHeaderValue() => $"{TypeConst} {Token}";
 }
