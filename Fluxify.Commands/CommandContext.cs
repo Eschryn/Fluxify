fa8d7820125adad7c7af2256577e0c506c1ffe17
Eschryn <8217655+Eschryn@users.noreply.github.com>
2026-03-15 13:34:48 +0100

    Introduce CommandReader for args parsing

--- Fluxify.Commands/CommandContext.cs
+++ Fluxify.Commands/CommandContext.cs
@@ -4,11 +4,20 @@ using Fluxify.Commands.TextCommand;
 
 namespace Fluxify.Commands;
 
-public class CommandContext(string prefix, Message message, IServiceProvider services)
+public class CommandContext
 {
-    public Message Message { get; } = message;
-    public IServiceProvider Services { get; } = services;
-    internal CommandTokenizer Tokenizer { get; } = CommandTokenizer.WithoutPrefix(prefix, message.Content);
+    public CommandContext(string prefix, Message message, IServiceProvider services)
+    {
+        Message = message;
+        Services = services;
+        Tokenizer = CommandTokenizer.WithoutPrefix(prefix, message.Content);
+        Reader = new CommandReader(Tokenizer);
+    }
+
+    public Message Message { get; }
+    public IServiceProvider Services { get; }
+    internal CommandTokenizer Tokenizer { get; }
+    public CommandReader Reader { get; }
     internal HashSet<string> PreconditionsFulfilled { get; set; } = [];
 
     public Task ReplyAsync(MessageDto message) => Message.ReplyAsync(message);
