4ba2e09c146bd52e4de94349d3d8e347288711fc
Eschryn <8217655+Eschryn@users.noreply.github.com>
2026-03-15 03:51:35 +0100

    Add Dm channel type & fix guild dto

--- Fluxify.Dto/Channels/ChannelType.cs
+++ Fluxify.Dto/Channels/ChannelType.cs
@@ -3,8 +3,10 @@ namespace Fluxify.Dto.Channels;
 public enum ChannelType
 {
     TextChannel = 0,
+    Dm = 1,
     VoiceChannel = 2,
     GroupDm = 3,
     Category = 4,
-    LinkChannel = 998
+    LinkChannel = 998,
+    DmPersonalNotes = 999
 }
