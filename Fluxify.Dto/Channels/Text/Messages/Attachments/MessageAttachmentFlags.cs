a50489c1f3ef9ed1c12a52f783d94dfc7671f761
Eschryn <git.elpul@eschryn.aleeas.com>
2026-03-15 17:20:18 +0100

    Add missing message flags to dto model

--- Fluxify.Dto/Channels/Text/Messages/Attachments/MessageAttachmentFlags.cs
+++ Fluxify.Dto/Channels/Text/Messages/Attachments/MessageAttachmentFlags.cs
@@ -17,6 +17,7 @@ namespace Fluxify.Dto.Channels.Text.Messages.Attachments;
 [Flags]
 public enum MessageAttachmentFlags : uint
 {
+    None = 0,
     IsSpoiler = 8,
     ContainsExplicitMedia = 16,
     IsAnimated = 32,
