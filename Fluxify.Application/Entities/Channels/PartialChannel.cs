397502b5c5d633338b0de51d413ec69e5a853aba
Eschryn <git.elpul@eschryn.aleeas.com>
2026-04-06 22:10:03 +0200

    Add partial channel

---
+++ Fluxify.Application/Entities/Channels/PartialChannel.cs
@@ -0,0 +1,18 @@
+using Fluxify.Core.Types;
+using Fluxify.Dto.Channels;
+
+namespace Fluxify.Application.Entities.Channels;
+
+public class PartialChannel : IChannel
+{
+    public Snowflake Id { get; init; }
+    public string Name { get; init; }
+    public ChannelType Type { get; init; }
+    public PartialChannelRecipient[]? Recipients { get; set; }
+    
+    public string ToString(string? format, IFormatProvider? formatProvider) => Id.ToString(format, formatProvider);
+    public object Clone()
+    {
+        return MemberwiseClone();
+    }
+}
---
+++ Fluxify.Application/Entities/Channels/PartialChannelRecipient.cs
@@ -0,0 +1,6 @@
+namespace Fluxify.Application.Entities.Channels;
+
+public class PartialChannelRecipient
+{
+    public required string Username { get; init; }
+}
