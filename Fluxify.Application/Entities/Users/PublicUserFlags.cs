ecb556c0be9e5d9cbe1314bb4c4c806b9775d980
Eschryn <git.elpul@eschryn.aleeas.com>
2026-08-01 17:43:59 +0200

    New public user flags

--- Fluxify.Application/Entities/Users/PublicUserFlags.cs
+++ Fluxify.Application/Entities/Users/PublicUserFlags.cs
@@ -15,12 +15,28 @@
 namespace Fluxify.Application.Entities.Users;
 
 [Flags]
-public enum PublicUserFlags : uint
+public enum PublicUserFlags : ulong
 {
-    Staff = 1,
-    CtpMember = 2,
-    Partner = 4,
-    BugHunter = 8,
-    FriendlyBot = 16,
-    FriendlyBotManualApproval = 32,
+    Staff = 1UL << 0,
+    CtpMember = 1UL << 1,
+    Partner = 1UL << 2,
+    BugHunter= 1UL << 3,
+    FriendlyBot= 1UL << 4,
+    FriendlyBotManualApproval = 1UL << 5,
+    Spammer = 1UL << 6,
+    HighGlobalRateLimit = 1UL << 33,
+    Deleted = 1UL << 34,
+    DisabledSuspiciousActivity = 1UL << 35,
+    SelfDeleted = 1UL << 36,
+    Disabled = 1UL << 38,
+    HasSessionStarted = 1UL << 39,
+    RateLimitBypass = 1UL << 47,
+    ReportBanned = 1UL << 48,
+    VerifiedNotUnderage = 1UL << 49,
+    HasDismissedPremiumOnboarding = 1UL << 51,
+    AppStoreReviewer = 1UL << 53,
+    StaffHidden = 1UL << 57,
+    AgeVerifiedAdult = 1UL << 60,
+    ForceInboundPhoneVerification = 1UL << 61,
+    NotSuspicious =  1UL << 62
 }
